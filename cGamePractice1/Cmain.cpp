1a9aa385151ae2466ff3d4e95a58a6b77a987625
sfc7 <124154607+sfc7@users.noreply.github.com>
2023-06-16 19:39:23 +0900

    update

--- cGamePractice1/Cmain.cpp
+++ cGamePractice1/Cmain.cpp
@@ -1,29 +1,13 @@
 #include <stdio.h>
 #include <Windows.h>
 
+#define MENUCOUNT 4
+
 enum Scene {
 	LOGO,
 	MENU,
 	STAGE
 };
-
-Scene currentScene;
-void Play();
-void SetScene(Scene newScene);
-
-
-const char* LogoImg[5];
-void Logo();
-void LogoRender();
-void LogoInitialize();
-
-void Menu();
-void MenuInitialize();
-
-void Stage();
-void StageInitialize();
-
-
 enum Color {
 	BLACK,
 	BLUE,
@@ -42,6 +26,47 @@ enum Color {
 	YELLOW,
 	WHITE,
 };
+enum Menu {
+	START,
+	Continue,
+	OPTION,
+	EXIT,
+};
+struct Obj {
+	int x;
+	int y;
+	Color color;
+	const char* shape;
+
+};
+Scene currentScene;
+void Play();
+void SetScene(Scene newScene);
+
+const char* logoImg[5];
+void LogoProgress();
+void LogoRender();
+void LogoInitialize();
+void LogoRelease() {
+
+}
+Obj* menuImg[16];
+Menu currentMENU;
+Obj* arrow;
+Obj* menu[MENUCOUNT];
+void MenuSetup(int number);
+void MenuProgress();
+void MenuInitialize();
+void MenuRender();
+
+void MenuRelease() {
+
+}
+
+void StageProgress();
+void StageInitialize();
+
+
 //커서숨기기
 void HideCursor();
 //좌표받고이동
@@ -56,7 +81,7 @@ int main() {
 	while (true) {
 		system("cls");
 		Play();
-		Sleep(10);
+		Sleep(50);
 	}
 	return 0;
 }
@@ -85,17 +110,20 @@ void ChangeColor(int color)
 }
 
 #pragma endregion
+#pragma region Play
+
 void Play() {
 	switch (currentScene) {
 	case LOGO:
-		Logo();
+		LogoProgress();
 		LogoRender();
 		break;
 	case MENU:
-		Menu();
+		MenuProgress();
+		MenuRender();
 		break;
 	case STAGE:
-		Stage();
+		StageProgress();
 		break;
 	default:
 		break;
@@ -117,8 +145,9 @@ void SetScene(Scene newScene) {
 		break;
 	}
 }
+#pragma endregion
 #pragma region Logo
-void Logo() {
+void LogoProgress() {
 	if (GetAsyncKeyState(VK_RETURN)) {
 		SetScene(MENU);
 	}
@@ -126,30 +155,119 @@ void Logo() {
 void LogoRender() {
 	for (int i = 0; i < 5; i++) {
 		MoveXY(10, 3 + i);
-		printf(LogoImg[i]);
+		printf(logoImg[i]);
 	}
 }
 void LogoInitialize() {
-	LogoImg[0] = "@@       @@";
-	LogoImg[1] = "@ @     @ @";
-	LogoImg[2] = "@  @   @  @";
-	LogoImg[3] = "@   @ @   @";
-	LogoImg[4] = "@    @    @";
+	logoImg[0] = "@@       @@";
+	logoImg[1] = "@ @     @ @";
+	logoImg[2] = "@  @   @  @";
+	logoImg[3] = "@   @ @   @";
+	logoImg[4] = "@    @    @";
 }
 #pragma endregion
 #pragma region Menu
-void Menu() {
-	if (GetAsyncKeyState(VK_RETURN)) {
-		SetScene(STAGE);
+void MenuProgress() {
+	if (GetAsyncKeyState(VK_UP)) {
+		if (arrow->y > menu[0]->y) {
+			arrow->y -= 2;
+			MenuSetup(-1);
+		}
 	}
-	printf("MENU");
+	if (GetAsyncKeyState(VK_DOWN)) {
+		if (arrow->y < menu[3]->y) {
+			arrow->y += 2;
+			MenuSetup(1);
+		}
+	}
+
+	if (GetAsyncKeyState(VK_SPACE)) {
+		switch (currentMENU) {
+		case START:
+			SetScene(STAGE);
+			break;
+		case OPTION:
+			break;
+		case EXIT:
+			break;
+		default:
+			break;
+		}
+	}
+
+	//if (GetAsyncKeyState(VK_RETURN)) {
+	//	SetScene(STAGE);
+	//}
 }
 void MenuInitialize() {
+	for (int i = 0; i < 16; i++) {
+		menuImg[i]->color = CYAN;
+	}
+	menuImg[0]->shape  = "MMMMMMMM               MMMMMMMMEEEEEEEEEEEEEEEEEEEEEENNNNNNNN        NNNNNNNNUUUUUUUU        UUUUUUUU";
+	menuImg[1]->shape = "M:::::::M             M:::::::ME::::::::::::::::::::EN:::::::N       N::::::NU::::::U        U::::::U";
+	menuImg[2]->shape  = "M::::::::M           M::::::::ME::::::::::::::::::::EN::::::::N      N::::::NU::::::U        U::::::U";
+	menuImg[3]->shape  = "M:::::::::M         M:::::::::MEE::::::EEEEEEEEE::::EN:::::::::N     N::::::NUU:::::U        U:::::UU";
+	menuImg[4]->shape  = "M::::::::::M       M::::::::::M  E:::::E       EEEEEEN::::::::::N    N::::::N U:::::U        U:::::U ";
+	menuImg[5]->shape  = "M:::::::::::M     M:::::::::::M  E:::::E             N:::::::::::N   N::::::N U:::::D        D:::::U ";
+	menuImg[6]->shape  = "M:::::::M::::M   M::::M:::::::M  E::::::EEEEEEEEEE   N:::::::N::::N  N::::::N U:::::D        D:::::U ";
+	menuImg[7]->shape  = "M::::::M M::::M M::::M M::::::M  E:::::::::::::::E   N::::::N N::::N N::::::N U:::::D        D:::::U ";
+	menuImg[8]->shape  = "M::::::M  M::::M::::M  M::::::M  E:::::::::::::::E   N::::::N  N::::N:::::::N U:::::D        D:::::U ";
+	menuImg[9]->shape  = "M::::::M   M:::::::M   M::::::M  E::::::EEEEEEEEEE   N::::::N   N:::::::::::N U:::::D        D:::::U ";
+	menuImg[10]->shape = "M::::::M    M:::::M    M::::::M  E:::::E             N::::::N    N::::::::::N U:::::D        D:::::U ";
+	menuImg[11]->shape = "M::::::M     MMMMM     M::::::M  E:::::E       EEEEEEN::::::N     N:::::::::N U::::::U      U::::::U ";
+	menuImg[12]->shape = "M::::::M               M::::::MEE::::::EEEEEEEE:::::EN::::::N      N::::::::N U:::::::UUUUUU:::::::U ";
+	menuImg[13]->shape = "M::::::M               M::::::ME::::::::::::::::::::EN::::::N       N:::::::N  UU::::::::::::::::UU  ";
+	menuImg[14]->shape = "M::::::M               M::::::ME::::::::::::::::::::EN::::::N        N::::::N    UU::::::::::::UU    ";
+	menuImg[15]->shape = "MMMMMMMM               MMMMMMMMEEEEEEEEEEEEEEEEEEEEEENNNNNNNN         NNNNNNN      UUUUUUUUUUUU     ";
+	
+	for (int i = 0; i < MENUCOUNT; i++) {
+		menu[i] = (Obj*)malloc(sizeof(Obj));
+		menu[i]->x = 25;
+		menu[i]->y = 22 + i * 2;
+		menu[i]->color = LIGHTGRAY;
+	}
+	menu[0]->shape = "   [ S T A R T ]";
+	menu[1]->shape = "[ C O N T I N U E ]";
+	menu[2]->shape = "  [ O P T I O N ]";
+	menu[3]->shape = "    [ E X I T]";
+
+	currentMENU = START;
+	arrow = (Obj*)malloc(sizeof(Obj));
+	arrow->x = 23;
+	arrow->y = 22;
+	arrow->color = LIGHTGRAY;
+	arrow->shape = "▶";
+
+	MenuSetup(0);
+}
+void MenuRender() {
+	for (int i = 0; i < 16; i++) {
+		MoveXY(5, 3 + i);
+		printf(menuImg[i]->shape);
+	}
+
+	for (int i = 0; i < MENUCOUNT; i++) {
+		MoveXY(menu[i]->x, menu[i]->y);
+		ChangeColor(menu[i]->color);
+		printf(menu[i]->shape);
+	}
+
+	MoveXY(arrow->x, arrow->y);
+	ChangeColor(arrow->color);
+	printf(arrow->shape);
 
 }
+void MenuSetup(int number) {
+	int index = currentMENU;
+	menu[index]->color = LIGHTGRAY;
+	index += number;
+	currentMENU = (Menu)index;
+	menu[index]->color = LIGHTGREEN;
+}
+
 #pragma endregion
 #pragma region Stage
-void Stage() {
+void StageProgress() {
 	if (GetAsyncKeyState(VK_RETURN)) {
 		SetScene(LOGO);
 	}
